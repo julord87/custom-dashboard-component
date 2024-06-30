6e140d83ae55d50f51b82ddc581f2e4ad5bd2810
Julian Martinez Gomez <julianmzg@gmail.com>
2024-06-30 10:18:50 -0300

    first commit

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Sidebar } from '../components';
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
+  return (
+    <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
+      <div className="flex">
+
+        <Sidebar />
+
+        <div className='p-2 w-full text-slate-900'>
+            {children}  
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
